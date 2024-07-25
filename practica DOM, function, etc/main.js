80f3a47a2b1b860c842110abfa76ea36612d1df0
EmanuelVargas11 <vargasriquelmege@gmail.com>
2024-07-25 17:28:40 -0300

    se creo un evento que cambia el color del fondo al parrafo

---
+++ practica DOM, function, etc/main.js
@@ -0,0 +1,11 @@
+const boton = document.querySelector('.cambiaColor')
+// console.log(boton);
+
+const parrafo = document.querySelector('.parrafo')
+parrafo.style.backgrounColor = 'coral'
+
+boton.addEventListener('click', () =>{
+
+    parrafo.style.backgroundColor = 'coral' 
+    
+} );
