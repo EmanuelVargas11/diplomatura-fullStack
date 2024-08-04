5b1259b8dd095d469011911a2f8c1cf840c4af29
EmanuelVargas11 <vargasriquelmege@gmail.com>
2024-08-04 12:41:36 -0300

    se creo la carpeta clases particulares, para ir guardando los ejercicios a parte de las clases de la diplomatura

---
+++ clases particulares/numeros divisibles, ejercicio if, else, for/main.js
@@ -0,0 +1,99 @@
+// for (let i = 1; i <= 15; i++){ 
+//     // for (let i = 20; i <= 40; i++) { 
+      
+     
+     
+     
+//     if (i %3 == 0 && i %5 == 0 ) { 
+//         console.log(`Aqui se muestra si es divisible por 3 y 5:  ${i}`); 
+         
+//     }else if (i %3 == 0) { 
+     
+//         console.log(`Aqui se muestra los divisibles por 3: ${i}`); 
+     
+//     }else if (i %5 == 0) { 
+         
+//             console.log(`Aqui se muestra si es divisible por 5:  ${i}`); 
+         
+     
+//     }else if(i %2 == 0) { 
+     
+//             console.log(`Aqui se muestra los divisibles por 2: ${i}`); 
+     
+//     }else { 
+//         console.log('no es divisible por los datos del indice', + i); 
+//     } 
+     
+     
+     
+//     }
+
+
+
+
+
+
+
+
+
+
+
+
+    //***********************************************************************************************************************/
+    //***********************************************************************************************************************/
+    //***********************************************************************************************************************/
+    //***********************************************************************************************************************/
+    //***********************************************************************************************************************/
+
+
+
+
+
+
+
+// Ahora con un array de numeros aleatorios, imprima el cuadrado de sus numeros
+
+
+
+
+    
+const numerosAleatorios = ["20", "15", "12314", "123", "125", "567", "8", "785", "23", "12", "34", "56", "5464", "13", "16", "25", "97", "14", "78", "31313", "3132132" ]
+
+const numerosCuadrados = 'Los siguiente que imprime son los cuadrados del array aleatorio: ' 
+
+
+for (let i = 0; i < numerosAleatorios.length; i++) {
+    
+
+    if (numerosAleatorios[i] %2 == 0) {
+
+
+        console.log(`${numerosCuadrados} ${numerosAleatorios[i]} `);              
+        
+    }
+    
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
