a9eea69f82dd04c138a2eb3c8c90d671b2e48f02
EmanuelVargas11 <vargasriquelmege@gmail.com>
2024-08-08 22:47:41 -0300

    se realizo en un archivo html y css, la aplicacion de js para modo oscuro

--- practica-con-flex-grid/main.js
+++ practica-con-flex-grid/main.js
@@ -5,49 +5,70 @@
 
 
 
-// practica con JS. Manipulacion de elementos
+// // practica con JS. Manipulacion de elementos
 
-let tituloPrinci = document.querySelector('div.titulo')
-// console.log(tituloPrinci);
-
-
-
-//  (innerText)se manipula la variable creada pero modifica en forma de texto
-tituloPrinci.innerText = '<h1>FLEX</h1>' 
+// let tituloPrinci = document.querySelector('div.titulo')
+// // console.log(tituloPrinci);
 
 
 
-// (innerTHTML)en esta opcion se manipula la variable creada pero pudiendo agregar nuevas etiquetas y texto dentro de las mismas
-
-// tituloPrinci.innerHTML = '<h2>Bienvenidos a la practica con JS</h2>'
-
+// //  (innerText)se manipula la variable creada pero modifica en forma de texto
+// tituloPrinci.innerText = '<h1>FLEX</h1>' 
 
 
 
-// cambiar estilo al titulo
+// // (innerTHTML)en esta opcion se manipula la variable creada pero pudiendo agregar nuevas etiquetas y texto dentro de las mismas
+
+// // tituloPrinci.innerHTML = '<h2>Bienvenidos a la practica con JS</h2>'
 
 
-    tituloPrinci.style.backgroundColor = '#e104ed'
-    tituloPrinci.style.color = 'black'
 
 
-    // creacion y eliminacion de nodos
-
-    // nueva etiqueta
-const nuevaEtiqueta = document.createElement('div')
-// texto de la etiqueta
-nuevaEtiqueta.innerText = 'Este es el nuevo div'
-document.body.append(nuevaEtiqueta) // se imprime en el HTML
-
-// le damos estilo a la nueva etiqueta
-
-nuevaEtiqueta.style.backgroundColor ='yellow'
-nuevaEtiqueta.style.borderRadius = '10px'
-nuevaEtiqueta.style.border = '2px solid'
+// // cambiar estilo al titulo
 
 
-// eliminando la nueva etiqueta 
+//     tituloPrinci.style.backgroundColor = '#e104ed'
+//     tituloPrinci.style.color = 'black'
 
-// document.querySelector('#nuevaEtiqueta')
-// nuevaEtiqueta.remove()
 
+//     // creacion y eliminacion de nodos
+
+//     // nueva etiqueta
+// const nuevaEtiqueta = document.createElement('div')
+// // texto de la etiqueta
+// nuevaEtiqueta.innerText = 'Este es el nuevo div'
+// document.body.append(nuevaEtiqueta) // se imprime en el HTML
+
+// // le damos estilo a la nueva etiqueta
+
+// nuevaEtiqueta.style.backgroundColor ='yellow'
+// nuevaEtiqueta.style.borderRadius = '10px'
+// nuevaEtiqueta.style.border = '2px solid'
+
+
+// // eliminando la nueva etiqueta 
+
+// // document.querySelector('#nuevaEtiqueta')
+// // nuevaEtiqueta.remove()
+
+
+
+
+
+
+const boton = document.querySelector('#botonModo')
+boton.innerText = 'Claro/Oscuro'
+
+const cuerpo = document.querySelector('#cueporBody')
+
+const tituloPrinci = document.querySelector('#tituloPrincipal')
+const navBar = document.querySelector('#navBar')
+
+
+
+boton.addEventListener('click', function() {
+    cuerpo.classList.toggle('modoOscuro')
+    tituloPrinci.classList.toggle('tituloModoOscuro')
+    navBar.classList.toggle('navBarOscuro')
+    
+})
